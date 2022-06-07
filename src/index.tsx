d89f16d9b78510a32aae09e4ab3d484afb8860a0
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-06-07 08:13:52 +0200

    Add and render pizza data

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App = () => {
-  return <div>Hello World</div>;
-};
+import App from './components/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
