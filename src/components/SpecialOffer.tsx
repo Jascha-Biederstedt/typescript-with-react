30455a79da1e494e703cb25e1aea425fa06b0e10
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-06-08 09:30:10 +0200

    Create render props component

--- src/components/SpecialOffer.tsx
+++ src/components/SpecialOffer.tsx
@@ -1,34 +1,42 @@
 import React from 'react';
 
 import { PizzaInterface } from '../types';
-import { withAddToCart, AddToCartProps } from './AddToCart';
+import { WithAddToCartProps } from './AddToCart';
 
 import styles from './SpecialOffer.module.css';
 
-interface Props extends AddToCartProps {
+interface Props {
   pizza: PizzaInterface;
 }
 
-const SpecialOffer: React.FC<Props> = ({ pizza, addToCart }) => {
-  const handleAddToCartClick = () => {
-    addToCart({ id: pizza.id, name: pizza.name, price: pizza.price });
-  };
-
+const SpecialOffer: React.FC<Props> = ({ pizza }) => {
   return (
     <div className={styles.container}>
       <span className={styles.special}>Special Offer</span>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
-      <button
-        className={styles.button}
-        type="button"
-        onClick={handleAddToCartClick}
-      >
-        Add to Cart
-      </button>
+      <WithAddToCartProps>
+        {({ addToCart }) => {
+          return (
+            <button
+              className={styles.button}
+              type="button"
+              onClick={() =>
+                addToCart({
+                  id: pizza.id,
+                  name: pizza.name,
+                  price: pizza.price,
+                })
+              }
+            >
+              Add to Cart
+            </button>
+          );
+        }}
+      </WithAddToCartProps>
     </div>
   );
 };
 
-export default withAddToCart(SpecialOffer);
+export default SpecialOffer;
